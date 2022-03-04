bf05a1151ab8b254655d8d21e8fbc06c16b284be
Tomáš Kůrka <82095105+Tom484@users.noreply.github.com>
2022-03-04 14:03:11 +0100

    New project

---
+++ scripts/questionsData.js
@@ -0,0 +1,44 @@
+const QUESTIONS = [
+  {
+    question: "Kdy se narodil Jules Verne?",
+    options: [
+      { option: "8. února 1928.", correct: false },
+      { option: "8. února 1528.", correct: false },
+      { option: "8. února 1828.", correct: true },
+    ],
+  },
+  {
+    question: "Kolik dětí měla jeho rodina?",
+    options: [
+      { option: "Jeho rodina měla 5 dětí, Jules byl nejstarší.", correct: true },
+      { option: "Jules byl jediným dítětem v rodině.", correct: false },
+      { option: "Jeho rodina měla 2 dětí, Jules byl mladším dítětem.", correct: false },
+    ],
+  },
+  {
+    question: "Co studoval Jules Verne?",
+    options: [
+      { option: "Jules odjel do Ruska studovat ekonomiku.", correct: false },
+      { option: "Jules odjel do Ameriky studovat hudbu.", correct: false },
+      { option: "Jules odjel do Paříže studovat práva.", correct: true },
+    ],
+  },
+  {
+    question: "Jakou knihu napsal?",
+    options: [
+      { option: "Malý Princ.", correct: false },
+      { option: "Cesta do středu Země.", correct: true },
+      { option: "Utrpení mladého Werthera.", correct: false },
+    ],
+  },
+  {
+    question: "Jaké byl národnosti?",
+    options: [
+      { option: "Je Francouz.", correct: true },
+      { option: "Je Němec.", correct: false },
+      { option: "Je Američan.", correct: false },
+    ],
+  },
+]
+
+export default QUESTIONS
